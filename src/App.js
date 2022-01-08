1ef16070df4bfe50c6be8004f3dbf9b5b70a027b
David Heredia <davis_heredia@hotmail.com>
2022-01-08 20:14:25 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
+import React, { useEffect } from "react";
 import { Route, Routes } from 'react-router-dom';
 import Pdf from './Pdf/Pdf';
 import Render from './Render/Render';
 
 
+const loader = document.querySelector(".preloader");
+
+const showLoader = () => loader.classList.remove("preloader");
+const addClass = () => loader.classList.add("loader-hide");
+
+
+
+
 function App() {
+
+  useEffect(() => {
+    showLoader();
+    addClass();
+  }, []);
+
   return (
     <div className='App'>
 
       <Routes>
-      <Route exact path="/" element={<Render />} />
+        <Route exact path="/" element={<Render />} />
         <Route exact path="/pdf" element={<Pdf />} />
-       
+
       </Routes>
 
     </div>
